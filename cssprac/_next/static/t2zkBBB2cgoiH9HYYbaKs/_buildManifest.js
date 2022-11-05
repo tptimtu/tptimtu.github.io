221effd5475054f2ec9775a65d8b7fb0adb83611
티피팀투 <101922071+tptimtu@users.noreply.github.com>
2022-11-06 07:24:43 +0900

    Delete cssprac directory

--- cssprac/_next/static/t2zkBBB2cgoiH9HYYbaKs/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/d45a705eaa85639f.css","static/chunks/pages/index-d6828815f5762d38.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
